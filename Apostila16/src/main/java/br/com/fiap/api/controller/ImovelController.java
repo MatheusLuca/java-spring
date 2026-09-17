01493241fd72172e2692a1e159fb865e7a28755b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-16 22:51:14 -0300

    Spring Boot

---
+++ Apostila16/src/main/java/br/com/fiap/api/controller/ImovelController.java
@@ -0,0 +1,16 @@
+package br.com.fiap.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("imoveis")
+public class ImovelController {
+
+    @GetMapping
+    public String dizerOla(){
+        return "Churros de chocolate";
+    }
+
+}
